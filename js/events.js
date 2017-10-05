68e70743d2de2bec422dd850b84a1a76af7c618d
Cristy Lucke <cristy.lucke@gmail.com>
2017-10-05 18:11:24 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -15,7 +15,7 @@ function frameIt() {
 function pressIt() {
   $('input').on('keydown', function(e) {
 //    if(e.shiftKey){
-      if(e.which === 71) {
+      if(e.which == 71) {
         alert("You pressed a CAPITAL G!");
 //      }
     }
