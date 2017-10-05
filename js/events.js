d99c3685ff34486f808972f17b1583a558932dbb
Cristy Lucke <cristy.lucke@gmail.com>
2017-10-05 17:35:08 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -7,15 +7,15 @@ function getIt() {
 }
 // Define a function frameIt that does not accept a parameter. The function should bind the load event that adds the class tasty to the image to add a red frame to the image.
 function frameIt() {
-  
+
 }
 // Define a function pressIt that does not accept a parameter. The function should bind a keydown event to the input field of the form that alerts a user when they have pressed the G key.
 function pressIt() {
-  
+
 }
 // Define a function submitIt that does not accept a parameter. The function should bind a submit event to the form that alerts "Your form is going to be submitted now.".
 function submitIt() {
-  
+
 }
 
 $(document).ready(function(){
