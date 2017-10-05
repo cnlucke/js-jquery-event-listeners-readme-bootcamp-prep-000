4dad70a9132508813a02d0f4316b29e14632e504
Cristy Lucke <cristy.lucke@gmail.com>
2017-10-05 17:55:12 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -6,7 +6,7 @@ function getIt() {
 }
 // Define a function frameIt that does not accept a parameter. The function should bind the load event that adds the class tasty to the image to add a red frame to the image.
 function frameIt() {
-  $('img#tasty').on('load', function() {
+  $('img').on('load', function() {
     $('img').addClass('tasty')
     $('img#tasty').css('border', "solid 2px red")
   })
